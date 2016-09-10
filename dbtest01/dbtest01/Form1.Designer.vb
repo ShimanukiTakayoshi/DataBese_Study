dc90ba9a5631598b2691eb95ccc2ef238eee44b2
shimanuki takayoshi <t-shima@db3.so-net.ne.jp>
2016-09-10 14:05:53 +0900

    フォーム作成

--- dbtest01/dbtest01/Form1.Designer.vb
+++ dbtest01/dbtest01/Form1.Designer.vb
@@ -22,9 +22,48 @@ Partial Class Form1
   'コード エディターを使って変更しないでください。
   <System.Diagnostics.DebuggerStepThrough()> _
   Private Sub InitializeComponent()
-    components = New System.ComponentModel.Container()
+    Me.components = New System.ComponentModel.Container()
+    Me.ButtonConnect = New System.Windows.Forms.Button()
+    Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+    Me.BindingSource1 = New System.Windows.Forms.BindingSource(Me.components)
+    CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+    CType(Me.BindingSource1, System.ComponentModel.ISupportInitialize).BeginInit()
+    Me.SuspendLayout()
+    '
+    'ButtonConnect
+    '
+    Me.ButtonConnect.Location = New System.Drawing.Point(8, 13)
+    Me.ButtonConnect.Name = "ButtonConnect"
+    Me.ButtonConnect.Size = New System.Drawing.Size(76, 29)
+    Me.ButtonConnect.TabIndex = 0
+    Me.ButtonConnect.Text = "接続"
+    Me.ButtonConnect.UseVisualStyleBackColor = True
+    '
+    'DataGridView1
+    '
+    Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+    Me.DataGridView1.Location = New System.Drawing.Point(8, 48)
+    Me.DataGridView1.Name = "DataGridView1"
+    Me.DataGridView1.RowTemplate.Height = 21
+    Me.DataGridView1.Size = New System.Drawing.Size(240, 150)
+    Me.DataGridView1.TabIndex = 1
+    '
+    'Form1
+    '
+    Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
     Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+    Me.ClientSize = New System.Drawing.Size(504, 261)
+    Me.Controls.Add(Me.DataGridView1)
+    Me.Controls.Add(Me.ButtonConnect)
+    Me.Name = "Form1"
     Me.Text = "Form1"
+    CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+    CType(Me.BindingSource1, System.ComponentModel.ISupportInitialize).EndInit()
+    Me.ResumeLayout(False)
+
   End Sub
 
+  Friend WithEvents ButtonConnect As Button
+  Friend WithEvents DataGridView1 As DataGridView
+  Friend WithEvents BindingSource1 As BindingSource
 End Class
